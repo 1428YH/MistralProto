61be53da9f96b5fad27191b1abcff05fd9be3a3f
Stef <romafon678@gmail.com>
2026-03-21 18:23:23 +0300

    fix: server.ts and config

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,12 +1,20 @@
-import { createServer } from "http";
+import { createServer, type Server } from "http";
+
+let _server: Server | null = null;
 
 export function runServer(html: string, port = 3000) {
-    const server = createServer((_req, res) => {
+    if (_server) {
+        _server.close(() => console.log("Previous server closed"));
+    }
+
+    _server = createServer((_req, res) => {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(html);
     });
-    server.listen(port, () => {
+
+    _server.listen(port, () => {
         console.log(`\nServer running at http://localhost:${port}\n`);
     });
-    return server;
+
+    return _server;
 }
